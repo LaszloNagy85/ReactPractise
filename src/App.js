57c3e4f0cbec0a470b050e2fb6511f99c03a3b28
LaszloNagy85 <n.laszlo.uk@gmail.com>
2020-06-15 11:53:57 +0200

    add newTodo card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
-// import Joke from "./components/Joke";
-// import Product from "./components/Product";
 import TodoItem from "./components/TodoItem";
+import NewTodo from "./components/NewTodo";
+// import todosData from "./data/todosData";
 
+// import Product from "./components/Product";
+// import Joke from "./components/Joke";
 // import jokesData from "./data/jokesData";
 // import productsData from "./data/vschoolProducts";
-import todosData from "./data/todosData";
 
 // function App() {
 //   const jokeComponents = jokesData.filter(joke => joke.question !== null &&  joke.question ? (joke.question.length > 50): false)
@@ -34,32 +35,92 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      todos : todosData,
+      todos: [],
+      isLoading: true,
+      isLoggedIn: false,
+      userName: "",
     };
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleUserNameInput = this.handleUserNameInput.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    fetch("https://api.jsonbin.io/b/5ee71a54ccc9877ac37bc396")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          isLoading: false,
+          todos: data || [],
+        });
+      });
+  }
+
+  handleUserNameInput(event) {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value,
+    });
   }
 
   handleChange(id) {
-    this.setState(prevState => {
-      const updatedTodos = prevState.todos.map(todo => {
+    this.setState((prevState) => {
+      const updatedTodos = prevState.todos.map((todo) => {
         if (todo.id === id) {
           return {
-            ...todo, completed: !todo.completed
-          }
+            ...todo,
+            completed: !todo.completed,
+          };
         }
         return todo;
-      })
-      return { todos : updatedTodos };
-    })
+      });
+      return { todos: updatedTodos };
+    });
+  }
+
+  handleClick() {
+    this.setState((prevState) => {
+      return { isLoggedIn: !prevState.isLoggedIn };
+    });
   }
 
   render() {
-    const todoItems = this.state.todos.map(todo =>
-      <TodoItem key={todo.id} item={todo} handleChange={this.handleChange}/>)
+    let todoItems = this.state.isLoading
+      ? []
+      : this.state.todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            item={todo}
+            handleChange={this.handleChange}
+          />
+        ));
+    let textForLogin = this.state.isLoggedIn ? "You are logged in as " + this.state.userName + "!" : "You are not logged in! Please enter your name: ";
+    let buttonText = this.state.isLoggedIn ? "Log out!" : "Log in!";
+    let todosListing = this.state.isLoading ? "Loading data! Please wait!" : todoItems;
+    let loginInputField = this.state.isLoggedIn ? null : (
+      <input
+        type="text"
+        className="user-name-input-field"
+        name="userName"
+        value={this.state.userName}
+        placeholder="Your username here!"
+        onChange={this.handleUserNameInput}
+      />
+    );
 
     return (
-      <div className="todo-list">
-        {todoItems}
+      <div>
+        <div className="App-header">
+          Todo App
+          <div className="login-text">{textForLogin}</div>
+          {loginInputField}
+          <button onClick={this.handleClick} className="login-button">
+            {buttonText}
+          </button>
+        </div>
+        <div className="todo-list">{todosListing}</div>
+        <NewTodo handleNewTodo={this.handleNewTodo}/>
       </div>
     );
   }
---
+++ src/components/NewTodo.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+class NewTodo extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+        firstName : "",
+        lastName : "",
+        todoDescription : "",
+        isDoneAlready : false,
+        priority : ""
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        const {name, value, type, checked} = event.target;
+        type === "checkbox" ? this.setState({ [name] : checked}) :
+                           this.setState({ [name] : value});
+    }
+
+    render() {
+        return (
+            <div className="todo-list">
+                <input type="text" name="firstName" value={this.state.firstName} placeholder="Enter your first name" onChange={this.handleChange}/>
+                <br/>
+                <input type="text" name="lastName" value={this.state.lastName} placeholder="Enter your first name" onChange={this.handleChange}/>
+                <br/>
+                <textarea name="todoDescription" value={this.state.todoDescription} placeholder="Type your todo description here!" onChange={this.handleChange} />
+                <br/>
+                <label><input type="checkbox" name="isDoneAlready" checked={this.state.isDoneAlraady} onChange={this.handleChange} />
+                Done already?</label>
+
+                <label><input type="radio" name="priority" value="high" checked={this.state.priority === "high"} onChange={this.handleChange} />
+                High priority!</label>
+
+                <label><input type="radio" name="priority" value="low" checked={this.state.priority === "low"} onChange={this.handleChange} />
+                Low priority!</label>
+
+                <p>{this.state.firstName} {this.state.lastName}</p>
+                <p>{this.state.todoDescription}</p>
+                <p>Is done already: {this.state.isDoneAlready ? "Yes!" : "No!"}</p>
+                <p>Priority: {this.state.priority}</p>
+            </div>
+        )
+    }
+
+}
+
+export default NewTodo;
